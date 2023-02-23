fe1a79da90a487f6d50b188a1bf84f247c31e799
RicMerces <ricardoliveira0703@gmail.com>
2023-02-22 21:12:59 -0300

    Deseja deletar ?

--- notify_layout/lib/app/screens/home/widget/add_button.dart
+++ notify_layout/lib/app/screens/home/widget/add_button.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 
 class AddButton extends StatelessWidget {
   const AddButton({Key? key}) : super(key: key);
@@ -24,7 +25,105 @@ class AddButton extends StatelessWidget {
           size: 30,
         ),
       ),
-      onPressed: () {},
+      onPressed: () => showDialog(
+        context: context,
+        builder: ((context) => Center(
+              child: Container(
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.circular(30),
+                ),
+                padding: const EdgeInsets.all(20),
+                height: 450,
+                width: 300,
+                child: Column(
+                  children: [
+                    SvgPicture.asset('assets/svg/SAVEORDELETE.svg'),
+                    const Text(
+                      'O que você deseja fazer com essa ',
+                      style: TextStyle(
+                        color: Color(0xff0D144B),
+                        fontSize: 16,
+                      ),
+                      textAlign: TextAlign.center,
+                    ),
+                    const Text(
+                      'Notificação ?',
+                      style: TextStyle(
+                        color: Color(0xff395BFF),
+                        fontSize: 20,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    const SizedBox(
+                      height: 20,
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Expanded(
+                          child: Container(
+                            decoration: const BoxDecoration(
+                              gradient: LinearGradient(
+                                begin: Alignment.topRight,
+                                end: Alignment.bottomLeft,
+                                colors: [
+                                  Color(0xff48F3A1),
+                                  Color(0xff31C87F),
+                                ],
+                              ),
+                              borderRadius: BorderRadius.only(
+                                topLeft: Radius.circular(30),
+                                bottomLeft: Radius.circular(30),
+                              ),
+                            ),
+                            child: TextButton(
+                              onPressed: () {},
+                              child: const Text(
+                                "Ressucitar",
+                                style: TextStyle(
+                                  fontSize: 20,
+                                  color: Colors.white,
+                                ),
+                              ),
+                            ),
+                          ),
+                        ),
+                        Expanded(
+                          child: Container(
+                            decoration: const BoxDecoration(
+                              gradient: LinearGradient(
+                                begin: Alignment.topLeft,
+                                end: Alignment.bottomRight,
+                                colors: [
+                                  Color(0xffFF4C48),
+                                  Color(0xffFF2E22),
+                                ],
+                              ),
+                              borderRadius: BorderRadius.only(
+                                topRight: Radius.circular(30),
+                                bottomRight: Radius.circular(30),
+                              ),
+                            ),
+                            child: TextButton(
+                              onPressed: () {},
+                              child: const Text(
+                                "Deletar",
+                                style: TextStyle(
+                                  fontSize: 20,
+                                  color: Colors.white,
+                                ),
+                              ),
+                            ),
+                          ),
+                        ),
+                      ],
+                    )
+                  ],
+                ),
+              ),
+            )),
+      ),
     );
   }
 }
